d493759924cdc96c9542b1de94ec9128d5a1fb58
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-11-04 11:48:13 -0500

    inquirer

--- game/game.js
+++ game/game.js
@@ -1,6 +1,23 @@
+import fs from 'fs'
+import inquirer from 'inquirer'
+
 const Pet = require('../lib/Pet')
 const User = require('../lib/User')
 const Food = require('../lib/Food')
 const Drink = require('../lib/Drink')
 
 
+inquirer.prompt([
+        {
+            type: "input",
+            name: "username",
+            message: "Welcome to Tamagotchi! Please enter your name!",
+        },
+    ])
+    .then((answers) => {
+     console.log(answers)
+    })
+    .catch((error) => {
+        if (error) throw error
+        else console.log("hey! no errors")
+    });
